a3f92d00b69c77c326e2d452ac6e0ad282c915f5
chris_podorsiki <chriszhang345@hotmail.com>
2023-03-02 03:25:56 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
-"""Top-level package for Ganblr."""
 
-__author__ = """Tulip Lab"""
-__email__ = 'jhzhou@tuliplab.academy'
-__version__ = '0.1.0'
 
 from .kdb import KdbHighOrderFeatureEncoder
 from .utils import get_demo_data
